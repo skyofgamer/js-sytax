dd070ce6f0eef59c0e5670ee9035712e13cecc85
skyofgamer <87489188+skyofgamer@users.noreply.github.com>
2023-03-25 14:41:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+console.log("i am console screen");
+
+var firstname = "Harmanveer ";
+console.log(firstname)
+
+var lastname ="singh"
+console.log(lastname);
+
+var fullname =firstname.concat(lastname)
+console.log(fullname);
+
+var lenthis =fullname.length;
+console.log(lenthis);
+
+var sentence ="i am Harmanveer singh"
+console.log(sentence)
+
+var test =sentence.replace("Harmanveer singh","Harmanveer akku");
+console.log(test)
+
+var upcase ="HARMANVEER SINGH"
+console.log(upcase);
+
+var lowcase =upcase.toLowerCase();
+console.log(lowcase)
+
+var sqrt =Math.sqrt(9)
+console.log(sqrt);
+
+var Max = Math.max(1,10)
+console.log(Max);
+
+var min = Math.min(1,10)
+console.log(min);
+
+var bignumber = 3.141592653589793
+console.log (bignumber)
+var tofixed = bignumber.toFixed(4)
+console.log(tofixed)
+
+var num1 = "6";
+console.log(typeof num1)
